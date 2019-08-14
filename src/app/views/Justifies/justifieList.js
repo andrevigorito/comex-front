38689004798a5b1d5084132326df7964142bff76
romeroalmeida <romerobm@gmail.com>
2019-08-14 12:01:57 -0300

    Update justifieList.js

--- src/app/views/Justifies/justifieList.js
+++ src/app/views/Justifies/justifieList.js
@@ -1,20 +1,19 @@
 import React from 'react';
 
+
 function JustifieList() {
   return (
     <div className="list-justificativas">
-      <div className="item">
-        <p>
-          Nulla vel placerat dolor. Etiam feugiat odio malesuada pellentesque
-          vulputate. Nulla convallis varius erat quis vestibulum. Donec vitae
-          ipsum vel elit porttitor porttitor quis eu sem.
-        </p>
-        <div className="user">
-          <p>Romero Almeida</p>
-          <p>12/07/2019 08:16:21</p>
-          <p>XO - AGENDAMENTO</p>
+      {/* {justifies.map(just => (
+        <div className="item">
+          <p>{just.description}</p>
+          <div className="user">
+            <p>{just.description}</p>
+            <p>{just.createdAt}</p>
+            <p>{just.tye}</p>
+          </div>
         </div>
-      </div>
+      ))} */}
     </div>
   );
 }
