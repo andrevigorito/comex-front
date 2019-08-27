41be39bc9b093885ec137eef9942dfac2c4ba507
romeroalmeida <romerobm@gmail.com>
2019-08-27 10:14:37 -0300

    alerta atualizado

--- src/app/views/components/FilterAlert.js
+++ src/app/views/components/FilterAlert.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import DatePicker, { registerLocale } from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 import ptBR from 'date-fns/locale/pt-BR';
 
@@ -12,80 +13,78 @@ class Filter extends Component {
     endDate: new Date(),
   };
 
-  handleChangeDate = date => {
-    this.setState({
-      date,
-    });
-  };
-
-  handleChangeStart = date => {
-    this.setState({
-      startDate: date,
-    });
-  };
-
-  handleChangeEnd = date => {
-    this.setState({
-      endDate: date,
-    });
-  };
-
   render() {
-    const { date, startDate, endDate } = this.state;
+    const {
+      startDate,
+      endDate,
+    } = this.state;
 
     return (
       <div className="filter-box">
-        <form action="">
-          <div className="item">
-            <label>Data de alteração:</label>
+        <form className="filtealert">
+          <Grid>
+            <Row>
+              <Col xs={12} md={4}>
+                <div className="item">
+                  <label>Data do Alerta:</label>
+                  <span>
+                    <DatePicker
+                      locale="pt-BR"
+                      selected={startDate}
+                      selectsStart
+                      startDate={startDate}
+                      endDate={endDate}
+                      onChange={this.handleChangeDateAta}
+                      dateFormat="dd/MM/yyyy"
+                      placeholderText="De"
+                    />
 
-            <DatePicker
-              locale="pt-BR"
-              selected={date}
-              selectsStart
-              onChange={this.handleChangeDate}
-              dateFormat="d MMMM , yyyy "
-            />
-          </div>
+                    <DatePicker
+                      locale="pt-BR"
+                      selected={endDate}
+                      selectsEnd
+                      startDate={startDate}
+                      endDate={endDate}
+                      onChange={this.handleChangeDateAtaFim}
+                      dateFormat="dd/MM/yyyy"
+                      placeholderText="Até"
+                    />
+                  </span>
+                </div>
+              </Col>
 
-          <div className="item">
-            <label>PO:</label>
-            <input type="text" id="idproduto" />
-          </div>
-          <div className="item">
-            <label>GR Inicial:</label>
+              <Col xs={12} md={3}>
+                <div className="item">
+                  <label>Mensagem:</label>
+                  <input
+                    type="text"
+                    id="idproduto"
+                    onChange={this.handleQueryInput}
+                    autoComplete="false"
+                  />
+                </div>
+              </Col>
+              <Col xs={12} md={2}>
+                <div className="item">
+                  <label>Status:</label>
+                  <select>
+                    <option>Não lido</option>
+                    <option>lido</option>
+                    <option>Todos</option>
+                  </select>
+                </div>
+              </Col>
 
-            <DatePicker
-              locale="pt-BR"
-              selected={startDate}
-              selectsStart
-              onChange={this.handleChangeStart}
-              startDate={startDate}
-              endDate={endDate}
-              dateFormat="d MMMM , yyyy "
-            />
-          </div>
-          <div className="item">
-            <label>GT Atual:</label>
-            {/* <input type="text" className="datepicker-here date" data-language="pt-BR" id="data-fim" /> */}
-            <DatePicker
-              locale="pt-BR"
-              selected={endDate}
-              selectsEnd
-              onChange={this.handleChangeEnd}
-              startDate={startDate}
-              endDate={endDate}
-              dateFormat="d MMMM , yyyy "
-              minDate={startDate}
-            />
-          </div>
-
-          <div className="item">
-            <label> &nbsp; </label>
-            <button type="button" className="btn">
-              Filtrar
-            </button>
-          </div>
+              <Col xs={12} md={3}>
+                <div className="item">
+                  <label> &nbsp; </label>
+                  <button type="submit" className="btn">
+                    Filtrar
+                  </button>
+                </div>
+              </Col>
+            </Row>
+          </Grid>
         </form>
       </div>
     );
