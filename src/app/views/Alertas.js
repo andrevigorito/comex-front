0ede16b921c46cd86f9fede5c9f351e3af964b78
Romero Almeida <romerobm@gmail.com>
2019-10-23 10:11:20 -0300

    Alert Count

--- src/app/views/Alertas.js
+++ src/app/views/Alertas.js
@@ -126,7 +126,7 @@ export default function Alertas({ useruuid }) {
             <p>Data Alerta</p>
             <p>Responsável</p>
             <p>Mensagem</p>
-            <p>Lido</p>
+            {/* <p>Lido</p> */}
             <p>Data Leitura</p>
             <p>Marcar como lido</p>
           </div>
@@ -144,14 +144,16 @@ export default function Alertas({ useruuid }) {
                 {new Date(alerta.createdAt).toLocaleString()}
               </p>
               <p className="responsible">{alerta.po.csr_name.toLowerCase()}</p>
-              <p className="po">{alerta.message}</p>
               <p className="po">
+                {alerta.message} <div className="box-count">33</div>
+              </p>
+              {/* <p className="po">
                 {alerta.user_alerts[0]
                   ? alerta.user_alerts[0].read
                     ? 'Sim'
                     : 'Não'
                   : ''}
-              </p>
+              </p> */}
               <p className="altered date">
                 {alerta.user_alerts[0]
                   ? alerta.user_alerts[0].read
@@ -159,26 +161,25 @@ export default function Alertas({ useruuid }) {
                     : ''
                   : ''}
               </p>
-
               <p>
                 {alerta.user_alerts[0] ? (
                   alerta.user_alerts[0].read ? (
                     ''
                   ) : (
-                    <button
-                      type="button"
-                      onClick={e => {
-                        e.stopPropagation();
-                        markAlertAsRead(alerta.uuid);
-                      }}
-                      className="btn"
-                    >
-                      Marcar como lido
+                      <button
+                        type="button"
+                        onClick={e => {
+                          e.stopPropagation();
+                          markAlertAsRead(alerta.uuid);
+                        }}
+                        className="btn"
+                      >
+                        Marcar como lido
                     </button>
-                  )
+                    )
                 ) : (
-                  ''
-                )}
+                    ''
+                  )}
               </p>
             </div>
           ))}
