f76de5530e82c8eed446a070419cfd25a09f254f
andrevigorito <34506311+andrevigorito@users.noreply.github.com>
2019-11-20 17:11:15 -0300

    Update envConfig.js

--- src/app/helpers/envConfig.js
+++ src/app/helpers/envConfig.js
@@ -1,6 +1,6 @@
 const configSystem = {
-  //URL_API: 'https://webcol-comex-test.herokuapp.com/',
-  URL_API: 'https://webcol.herokuapp.com/',
+  URL_API: 'https://webcol-comex-test.herokuapp.com/',
+  //URL_API: 'https://webcol.herokuapp.com/',
   // URL_API: 'http://localhost:4000/',
   JWT_SECRET: '95c5ebbd38cd08f1f45ebef2d479349d',
 };
