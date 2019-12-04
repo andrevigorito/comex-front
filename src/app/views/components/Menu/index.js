9fec8ffc5e782eebc149b0a5f3a4c40205595810
andrevigorito <34506311+andrevigorito@users.noreply.github.com>
2019-12-04 16:07:08 -0300

    paineis label change

--- src/app/views/components/Menu/index.js
+++ src/app/views/components/Menu/index.js
@@ -61,7 +61,7 @@ class Menu extends Component {
                   Import
                 </Link> */}
                 <Link onClick={this.btnMenu} to="/gerencial">
-                  Gerencial{' '}
+                  Painéis{' '}
                 </Link>
                 <Link onClick={this.btnMenu} to="/operacional">
                   Operacional{' '}
